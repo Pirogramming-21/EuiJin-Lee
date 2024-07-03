8c46ae73b66845eb3fe41b64c3c815d70b60d7a8
Eui Jin Lee <Dowha132@yonsei.ac.kr>
2024-07-03 17:54:19 +0900

    JavaScript-display items

--- 쇼핑몰 과제/main.js
+++ 쇼핑몰 과제/main.js
@@ -1,12 +1,25 @@
 function loadItems(){
-    return fetch('data/dat.json')
+    return fetch('data/data.json')
     .then(response => response.json())
     .then(json =>json.items);
+}
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item){
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail"/>
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
 }
 
 loadItems()
 .then(items =>{
     displayItems(items);
-    setEventListeners(items)
+    //setEventListeners(items)
 })
 .catch(console.log);
