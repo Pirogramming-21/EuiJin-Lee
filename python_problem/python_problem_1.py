688c0c22c02ec09a1ca357a8bfaa4df132535767
Eui Jin Lee <Dowha132@yonsei.ac.kr>
2024-07-06 00:16:53 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -4,9 +4,8 @@ def get_valid_input():
     while True:
         try:
             count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능):"))
-            if count not in [1,2,3]:
-                print("1,2,3 중 하나만 입력하세요")
-            
+            if count not in [1, 2, 3]:
+                print("1, 2, 3 중 하나만 입력하세요")
             else:
                 return count
         except ValueError:
@@ -16,17 +15,20 @@ def play_turn(player):
     global num
     count = get_valid_input()
     for i in range(count):
-        num +=1
-        print(f"{player}:{num}")
+        num += 1
+        print(f"{player}: {num}")
         if num >= 31:
             return True
     return False
 
-players = ['playerA','playerB']
-current_player = 0
+def brGame():
+    players = ['playerA', 'playerB']
+    current_player = 0
 
-while True:
-    if play_turn(players[current_player]):
-        print(f"{players[1-current_player]} Win!")
-        break
-    current_player = 1 - current_player
+    while True:
+        if play_turn(players[current_player]):
+            print(f"{players[1 - current_player]} Win!")
+            break
+        current_player = 1 - current_player
+
+brGame()
