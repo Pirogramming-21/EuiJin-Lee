bf267445c8f937c2a78904dbcce611498788381f
Eui Jin Lee <Dowha132@yonsei.ac.kr>
2024-07-05 21:26:19 +0900

    const Id connect

---
+++ Javascript 스톱워치 과제/script.js
@@ -0,0 +1,13 @@
+const display =document.getElementById('display');
+const startStopBtn =document.getElementById('startStop');
+const resetBtn =document.getElementById('reset');
+const recordBtn =document.getElementById('record');
+const startTimerBtn =document.getElementById('startTimer');
+const selectAllBtn =document.getElementById('selectAll');
+const deleteSelectedBtn =document.getElementById('deleteSelected');
+const deleteAllBtn =document.getElementById('deleteAll');
+const recordList =document.getElementById('recordList');
+const timerInput =document.getElementById('timerInput');
+
+
+
