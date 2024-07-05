88ec2bcd6c0325507017c985c1d8c98229e24ae7
Eui Jin Lee <Dowha132@yonsei.ac.kr>
2024-07-05 22:01:15 +0900

    startStop Btn JS

--- Javascript 스톱워치 과제/script.js
+++ Javascript 스톱워치 과제/script.js
@@ -1,3 +1,9 @@
+let isRunning = false;
+let seconds =0;
+let minutes =0;
+let hours =0;
+
+
 const display =document.getElementById('display');
 const startStopBtn =document.getElementById('startStop');
 const resetBtn =document.getElementById('reset');
@@ -9,5 +15,35 @@ const deleteAllBtn =document.getElementById('deleteAll');
 const recordList =document.getElementById('recordList');
 const timerInput =document.getElementById('timerInput');
 
+function startStop(){
+    if(isRunning){
+        clearInterval(timer);
+        startStopBtn.textContent = '시작';
+    }
+    else {
+        timer = setInterval(updateDisplay, 1000);
+        startStopBtn.textContent = '정지';
+    }
+    isRunning = !isRunning;
+}
+
+function updateDisplay(){
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes === 60){
+            minutes = 0;
+            hours++;
+        }
+    }
+    display.textContent = formatTime(hours, minutes, seconds);
+}
+
+function formatTime(h, m, s) {
+    return `${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
+}
 
 
+
+startStopBtn.addEventListener('click', startStop);
